3cccce0dc075d424d3ba28f003fbec48ff577e0a
FelipeRavaCardona <feliperavacardona@hotmail.com>
2024-04-16 22:14:02 -0300

    removing unwanted comments

--- app_server.py
+++ app_server.py
@@ -68,7 +68,6 @@ def handle_file(sender_ip, sender_port, message_data):
             'code': 1,
             'message': f"{message_data['recipient']} is not online."
         })
-    # TODO: Send file and message to recipient
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sender = find_user_by_address(sender_ip, sender_port)
         print(f"File sent from {sender['nickname']} to {recipient['nickname']}")
@@ -116,7 +115,6 @@ def handle_received(data, sender_ip, sender_port):
             return f"OPCODE '{message.OPCODE} is not known."
 
 def start_server_udp(host, port):
-    # TODO: follow commented code?
     global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
@@ -128,20 +126,3 @@ def start_server_udp(host, port):
         server_socket.sendto(response.encode(), address)
 
 start_server_udp('localhost', 3000)
-# import socket
-
-# def start_server(host, port):
-#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     server_socket.bind((host, port))
-#     print(f"UDP server listening on {host}:{port}")
-
-#     while True:
-#         data, address = server_socket.recvfrom(1024)
-#         response = handle_message(data, address)
-#         server_socket.sendto(response.encode(), address)
-
-# # Example usage:
-# if __name__ == "__main__":
-#     HOST = 'localhost'  # Change this to your server's IP address
-#     PORT = 3000        # Choose a port for your server
-#     start_server(HOST, PORT)
