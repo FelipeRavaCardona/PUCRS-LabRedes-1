12869976cdcc01b3280c0a5055f58376768abd6c
FelipeRavaCardona <feliperavacardona@hotmail.com>
2024-04-25 11:25:25 -0300

    commenting code

--- app_server.py
+++ app_server.py
@@ -2,9 +2,12 @@ import json
 import threading
 from udp import udp_server as server
 
+#Lista de usuários comunicando com o servidor
 users = []
 
 def find_user_by_address(ip, port):
+    # Procura na lista de usuários por um usuário com um ip e porta específicos
+    # retorna None caso não encontre
     global users
     for user in users:
         if user['ip'] == ip and user['port'] == port:
@@ -12,12 +15,17 @@ def find_user_by_address(ip, port):
     return None
 
 def find_user_by_nickname(nickname):
+    # Procura na lista de usuários por um usuário com um nickname específico,
+    # Retorna None caso não encontre
     for user in users:
         if user['nickname'] == nickname:
             return user
     return None
 
 def handle_registration(nickname, sender_ip, sender_port):
+    # Cadastra um usuário na aplicação
+    # Retorna code 1 caso ja exista um usuário com o nickname escolhido
+    # Retorna code 0 caso o cadastro seja feito com sucesso
     if find_user_by_nickname(nickname):
         return json.dumps({
             'OPCODE': 1,
@@ -38,6 +46,9 @@ def handle_registration(nickname, sender_ip, sender_port):
     })
 
 def handle_message(sender_ip, sender_port, message_data):
+    # Recebe a mensagem de um usuário e envia para outro
+    # Retorna code 1 para o usuário que enviou a mensagem caso a mensagem não possa ser entregue ao remetente
+    # Envia com code 0 para o remetente caso tudo dê certo 
     recipient = find_user_by_nickname(message_data['recipient'])
     if not recipient:
         return json.dumps({
@@ -60,6 +71,9 @@ def handle_message(sender_ip, sender_port, message_data):
     })
 
 def handle_file(sender_ip, sender_port, message_data):
+    # Recebe a mensagem e um arquivo de um usuário e envia para outro
+    # Retorna code 1 para o usuário que enviou a mensagem caso a mensagem e o arquivo não possam ser entregues ao remetente
+    # Envia com code 0 para o remetente caso tudo dê certo
     recipient = find_user_by_nickname(message_data['recipient'])
     if not recipient:
         return json.dumps({
@@ -83,6 +97,9 @@ def handle_file(sender_ip, sender_port, message_data):
     })
 
 def handle_disconnect(sender_ip, sender_port):
+    # Lida com a mensagem avisando do encerramento da conexão
+    # Retorna code 0 caso o usuário seja desconectado com sucesso
+    # Retorna code 1 caso o usuário não exista(não esteja mais conectado) na aplicação
     sender = find_user_by_address(sender_ip, sender_port)
     if sender:
         users.remove(sender)
@@ -99,22 +116,29 @@ def handle_disconnect(sender_ip, sender_port):
     })
 
 def handle_received():
+    # Lida com todas mensagens que chegam
     while True:
         data, address = server.message_queue.get()
         message = json.loads(data.decode('utf-8'))
         sender_ip, sender_port = address
         match message['OPCODE']:
             case 1:
+                # OPCODE 1, registrar usuário
                 server.send_data(handle_registration(message['nickname'], sender_ip, sender_port), address)
             case 2:
+                # OPCODE 2, enviar mensagem de um usuário para outro
                 server.send_data(handle_message(sender_ip, sender_port, message), address)
             case 3:
+                # OPCODE 3, enviar mensagem e arquivo de um usuário para outro
                 server.send_data(handle_file(sender_ip, sender_port, message), address)
             case 4:
+                # OPCODE 4, encerramento de comunicação
                 server.send_data(handle_disconnect(sender_ip, sender_port), address)
             case _:
+                # OPCODE desconhecido
                 server.send_data(f"OPCODE '{message.OPCODE} is not known.", address)
 
+# Inicia thread the receber mensagens do UDP
 receiving_thread = threading.Thread(target=server.receive_data)
 receiving_thread.daemon = True
 receiving_thread.start()
--- udp/udp_server.py
+++ udp/udp_server.py
@@ -1,21 +1,25 @@
-import json
 import queue
 import socket
 
+#Definição da configuração UDP
 SERVER_ADDRESS = ('localhost', 3000)
 BUFF_SIZE = 2048
 
+#Inicialização do socket UDP
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(SERVER_ADDRESS)
 
+#Criação da fila de mensagens recebidas
 message_queue = queue.Queue()
 
 print(f"UDP server listening on {SERVER_ADDRESS[0]}:{SERVER_ADDRESS[1]}")
 
 def send_data(message, address):
+    #Envia mensagens aos usuários
     server_socket.sendto(message.encode(), address)
 
 def receive_data():
+    #Recebe mensagens dos usuários
     while True:
         data, address = server_socket.recvfrom(BUFF_SIZE)
         message_queue.put((data, address))
